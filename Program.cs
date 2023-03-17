c76c5c14594fb34b75736b9b8319f645f4b29155
Andrea-Baggio <andreabaggio800@gmail.com>
2023-03-17 14:19:52 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int[] myArray = { 1, 2, 3, 4, 5 };
+StampaArray(myArray);
+
+static void StampaArray(int[] num)
+{
+    Console.Write("[");
+    for (int i = 0; i < num.Length; i++)
+    {
+        Console.Write("elemento " + num[i]);
+        if (i != num.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+
