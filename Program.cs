382b5dd52988c0e72e6b7c1cbc14d27c7c2694ea
Andrea-Baggio <andreabaggio800@gmail.com>
2023-03-17 15:31:53 +0100

    Stampare l’array di numeri fornito a video

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿void StampaArray(int[] array)
+﻿int[] array = { 2, 6, 7, 5, 3, 9 };
+Console.WriteLine(string.Join(", ", array));
+
+StampaArray(array);
+void StampaArray(int[] array)
 {
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
@@ -12,32 +16,32 @@
     Console.Write("]");
 }
 
-int Quadrato(int numero)
-{
-    int result = numero * numero;
-    return result;
-}
+//int Quadrato(int numero)
+//{
+//    int result = numero * numero;
+//    return result;
+//}
 
 
-int[] ElevaArrayAlQuadrato(int[] array)
-{
-    int[] copiaArray = (int[])array.Clone();
+//int[] ElevaArrayAlQuadrato(int[] array)
+//{
+//    int[] copiaArray = (int[])array.Clone();
     
-    for (int i = 0; i < copiaArray.Length; i++)
-    {
-        copiaArray[i] = copiaArray[i] * copiaArray[i];
-    }
+//    for (int i = 0; i < copiaArray.Length; i++)
+//    {
+//        copiaArray[i] = copiaArray[i] * copiaArray[i];
+//    }
 
-    return copiaArray;
-}
+//    return copiaArray;
+//}
 
 
-int sommaElementiArray(int[] array)
-{
-    int sum = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        sum += array[i];
-    }
-    return sum;
-}
+//int sommaElementiArray(int[] array)
+//{
+//    int sum = 0;
+//    for (int i = 0; i < array.Length; i++)
+//    {
+//        sum += array[i];
+//    }
+//    return sum;
+//}
